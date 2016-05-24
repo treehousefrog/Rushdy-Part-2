9af7b5aadab58d4902750c2dd2c67525b19c1ae0
treehousefrog <rushdy_worm@hotmail.com>
2016-05-24 17:52:14 +0100

    Upload 1

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Controls : MonoBehaviour {
+    public Rigidbody2D rb;
+    public float movespeed;
+    public float jumpheight;
+    public bool moveright;
+    public bool moveleft;
+    public bool jump;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool onGround;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void FixedUpdate()
+    {
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (onGround)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpheight);
+            }
+        }
+
+        if (jump)
+        {
+            if (onGround)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpheight);
+            }
+            jump = false;
+        }
+
+        if (moveright)
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+        }
+        if (moveleft)
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+        }
+
+    }
+    
+}
